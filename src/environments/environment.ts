e623c75a4e64b923f3f6bc2c1a2567a58c6020dc
kingIzi <scott.izidore@gmail.com>
2024-06-29 12:18:11 +0300

    ..

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,23 +5,4 @@ export const environment = {
   baseUrl: 'http://192.168.100.50:98',
   //baseUrl: URL, //dynamicUrl
   //isDebug:true,
-
-  //vendor headers
-  // "headers": [
-  //   "Customers",
-  //   "Company Users",
-  //   "Invoice details",
-  //   "Generated invoice",
-  //   "Language"
-  // ]
-
-  // "generatedInvoices": [
-  //   "Sno",
-  //   "Invoice No",
-  //   "Invoice Date",
-  //   "Customer name",
-  //   "Total Amount",
-  //   "Remarks",
-  //   "Status"
-  // ],
 };
