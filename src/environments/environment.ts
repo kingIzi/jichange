b17a6e273bff19a73f15b022a7328693aad287fe
Izi Kapampa <56266013+kingIzi@users.noreply.github.com>
2024-08-03 13:02:49 +0300

    ..

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  //baseUrl: 'http://192.168.100.50:98',
-  baseUrl: 'https://localhost:44310',
+  baseUrl: 'http://192.168.100.50:98',
+  //baseUrl: 'https://localhost:44310',
   //baseUrl: URL, //dynamicUrl
   //isDebug:true,
 };
